df667c65d084862ac74262678f60c086fbbe54fe
Gian Luca <55563130+GLPG35@users.noreply.github.com>
2026-01-28 20:39:45 -0300

    v1.2.1
    Fixed header issue on home page

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -9,14 +9,12 @@ import styles from './styles.module.scss'
 import type { EmblaCarouselType } from 'embla-carousel'
 import useEmblaCarousel from 'embla-carousel-react'
 import Autoplay from 'embla-carousel-autoplay'
-import { useInView } from 'motion/react'
 import { $language, useSiteStore } from '../../store/useSiteStore'
 import { useSsrStore } from '../../hooks/useSsrStore'
 
 const Home = () => {
 	const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay({ stopOnInteraction: false })])
 	const ref = useRef<HTMLElement>(null)
-	const inView = useInView(ref, { margin: '0px 0px -50% 0px', initial: false })
 	const language = useSsrStore($language)
 	const setDark = useSiteStore(state => state.setDark)
 	const setInView = useSiteStore(state => state.setInView)
@@ -68,6 +66,8 @@ const Home = () => {
 
 		return () => {
 			window.removeEventListener('scroll', handleScroll)
+
+			setInView(false)
 		}
 	}, [])
 
