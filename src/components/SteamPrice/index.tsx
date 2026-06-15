c51993e076eea87802b8498472dc5c328838f537
Gian Luca <55563130+GLPG35@users.noreply.github.com>
2026-06-15 13:08:57 -0300

    v2.3.1
    Add custom component to fetch data from Steam API

--- src/components/SteamPrice/index.tsx
+++ src/components/SteamPrice/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-const SteamPrice = ({ appId, text, smallText }: { appId: number, text: string, smallText: string }) => {
+const SteamPrice = ({ appId, text, smallText, loadingText, notFoundText }: { appId: number, text: string, smallText: string, loadingText: string, notFoundText: string }) => {
 	const [rootData, setRootData] = useState<{ [key: number]: { data: { price_overview: { discount_percent: number, initial_formatted: string, final_formatted: string } } } }|null>()
 	
 	useEffect(() => {
@@ -20,23 +20,15 @@ const SteamPrice = ({ appId, text, smallText }: { appId: number, text: string, s
 		})
 	}, [])
 
-	if (rootData === undefined) return <span>Loading price...</span>
+	if (rootData === undefined) return <span>{loadingText}</span>
 
-	if (rootData === null) return <span>Price not found</span>
+	if (rootData === null) return <span>{notFoundText}</span>
 	
 	const { [appId]: { data: { price_overview: { discount_percent: discount, initial_formatted: initial, final_formatted: final } } } } = rootData
 
 	return <>
-		{rootData === undefined ?
-			<span>Loading price...</span>
-		: rootData === null ?
-			<span>Price not found</span>
-		:
-			<>
-				<span>{discount > 0 ? text : ''} {discount > 0 ? <s>{initial}</s> : ''} {final}</span>
-				{discount === 0 && <span className="small">{smallText}</span>}
-			</>	
-		}
+		<span>{discount > 0 ? text : ''} {discount > 0 ? <s>{initial}</s> : ''} {final}</span>
+		{discount === 0 && <span className="small">{smallText}</span>}
 	</>
 }
 
